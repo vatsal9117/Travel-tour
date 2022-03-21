9bfb4948b3788b21c584926dbcb2ab038fa1e785
Vatsal Dalal <67739998+vatsal9117@users.noreply.github.com>
2022-03-21 11:46:48 +0530

    Add files via upload

---
+++ src/pages/SignUp.js
@@ -0,0 +1,20 @@
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import Registration from "../components/Registartion";
+
+
+const SignUp = () => {
+    return ( 
+        <div className="signup">
+            <Header />
+            <NavBar />
+            <Registration />
+            <Footer />
+            <div>
+                
+            </div>
+        </div>
+     );
+}
+export default SignUp;
